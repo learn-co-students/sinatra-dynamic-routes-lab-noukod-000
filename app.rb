ed2c8095f1689b02bb6dc3f7e259cfc166e81d64
Saint-Jean Germila <germila@noukod.com>
2019-01-15 17:18:57 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,33 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+get '/reversename/:name' do
+@reversename = @params[:name].reverse
+
+end
+
+get '/square/:number' do
+  @square= @params[:number].to_i*@params[:number].to_i
+  "#{square}"
+end
+
+get '/say/:number/:phrase' do
+
+end
+
+get '/say/:word1/:word2/:word3/:word4/:word5' do
+
+end
+
+get '/:operation/:number1/:number2' do
+  @sum =@params[:number1].to_i+@params[:number2].to_i
+  "#{sum}"
+
+  @product =@params[:number1].to_i*@params[:number2].to_i
+  "#{product}"
+
+  @div =@params[:number1].to_i/@params[:number2].to_i
+  "#{div}"
+end
 
 end
