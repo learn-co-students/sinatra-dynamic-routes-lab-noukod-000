fa8bc7ff2bd7c37051d703b5e387b6c630d1446e
Andy Feidje Joseph <ingandy75@gmail.com>
2019-06-28 19:19:23 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,24 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+  get '/reversename/:name' do
+    erb :reversename
+  end
+
+get '/square/:number' do
+  erb :square
+end
+
+get '/say/:number/:phrase' do
+  erb :multiply_string
+end
+
+get '/say/:word1/:word2/:word3/:word4/:word5' do
+  erb :all_five_words
+end
+
+get '/:operation/:number1/:number2' do
+  erb :operation
+end
 
 end
