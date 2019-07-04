907c11ef879504a4fec26b4dd7bd20fe0843f9d7
Alexandro Disla <alexandrodisla@hotmail.com>
2019-07-04 17:46:49 +0000

    Done.

--- app.rb
+++ app.rb
@@ -3,28 +3,28 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
   get '/reversename/:name' do
-    @namereverse = params[:name]
-    "#{@namereverse.reverse}"
+    namereverse = params[:name]
+    "#{namereverse.reverse}"
   end
 
   get '/square/:number' do
-    @square = params[:number]
-    "#{@square.to_i ** 2}"
+    square = params[:number]
+    "#{square.to_i ** 2}"
   end
 
   get '/say/:number/:phrase' do
-    @number = params[:number]
-    @phrase = params[:phrase]
-    "#{@phrase*@number.to_i}"
+    number = params[:number]
+    phrase = params[:phrase]
+    "#{phrase*number.to_i}"
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
-    @word1 = params[:word1]
-    @word2 = params[:word2]
-    @word3 = params[:word3]
-    @word4 = params[:word4]
-    @word5 = params[:word5]
-    "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}."
+    word1 = params[:word1]
+    word2 = params[:word2]
+    word3 = params[:word3]
+    word4 = params[:word4]
+    word5 = params[:word5]
+    "#{word1} #{word2} #{word3} #{word4} #{word5}."
   end
 
   get '/:operation/:number1/:number2' do
